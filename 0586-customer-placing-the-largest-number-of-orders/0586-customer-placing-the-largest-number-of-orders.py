d32aabe66b07fbc1511d7130d157a2c8cbbcfde3
jifujifrin67-commits <jifujifrin67@gmail.com>
2026-07-06 14:22:46 +0530

    Time: 307 ms (31.15%), Space: 67.3 MB (31.85%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.py
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.py
@@ -5,4 +5,3 @@ def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
     orders['counts'] = orders.groupby('customer_number').transform('size')
 
     return orders.sort_values(by = ['counts']).tail(1).iloc[:,[1]]
-
