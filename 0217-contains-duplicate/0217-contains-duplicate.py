eed495f18951379a27c1d23338e3f9def9289731
jifu jifrin <jifujifrin67@gmail.com>
2026-09-10 10:44:56 +0530

    Time: 0 ms (100%), Space: 32.3 MB (54.68%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        hashset = set(nums)
+        if len(hashset) == len(nums):
+            return False
+        else:
+            return True
+
+
+
+
+
